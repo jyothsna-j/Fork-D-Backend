0a5dedf49a92d37e7357704adea57cd90c588b2e
Jyothsna Jairam <mjr.jyothsna@gmail.com>
2025-04-19 12:19:02 +0530

    bug fix

--- src/main/java/com/forkd/forkd_backend/controller/uEngagePojos/TrackTaskCallbackResponse.java
+++ src/main/java/com/forkd/forkd_backend/controller/uEngagePojos/TrackTaskCallbackResponse.java
@@ -10,6 +10,22 @@ public class TrackTaskCallbackResponse {
 		this.status = status;
 		this.message = message;
 	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 	
 	
 }
