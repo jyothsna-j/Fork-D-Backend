fe4c92eb659f353ec48ff42e9ba135ca01c567a2
Jyothsna Jairam <mjr.jyothsna@gmail.com>
2025-05-04 13:14:17 +0530

    id addition

--- src/main/java/com/forkd/forkd_backend/utils/StoreIdMapping.java
+++ src/main/java/com/forkd/forkd_backend/utils/StoreIdMapping.java
@@ -15,6 +15,7 @@ public class StoreIdMapping {
         Map<Integer, String> map = new HashMap<>();
         map.put(2, "63136");
         map.put(3, "63192");
+        map.put(4, "63486");
         // Add more mappings here
         ID_MAP = Collections.unmodifiableMap(map);
     }
