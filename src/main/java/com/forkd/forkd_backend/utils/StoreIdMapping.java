3f8199b0d0eb9a14f026c3b8541bad5b8f30486b
Jyothsna Jairam <mjr.jyothsna@gmail.com>
2025-05-22 05:25:00 +0530

    storeidupdate

--- src/main/java/com/forkd/forkd_backend/utils/StoreIdMapping.java
+++ src/main/java/com/forkd/forkd_backend/utils/StoreIdMapping.java
@@ -15,8 +15,8 @@ public class StoreIdMapping {
         Map<Integer, String> map = new HashMap<>();
         map.put(2, "63136");
         map.put(3, "63192");
-        map.put(4, "63486");
-        map.put(6, "63714");
+        map.put(1, "63486");
+        map.put(4, "63714");
         // Add more mappings here
         ID_MAP = Collections.unmodifiableMap(map);
     }
